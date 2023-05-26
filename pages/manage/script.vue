91e308449b4771222eda4eccddfc5be8452f5374
葉奕緯 <aaa24295234@gmail.com>
2023-05-26 17:21:31 +0800

    code script

--- pages/manage/script.vue
+++ pages/manage/script.vue
@@ -2,10 +2,10 @@
     <NuxtLayout name="custom">
         <div class="manage">
             <div class="manage-between">
-                <div class="manage-head">註冊碼管理</div>
+                <div class="manage-head">劇本教材管理</div>
                 <div class="manage-row">
                     <div class="manage-num">項目數量：10</div>
-                    <div @click="isShowAdd = true" class="manage-create">+ 新增註冊碼</div>
+                    <div @click="addUser" class="manage-create">+ 建立劇本教材</div>
                 </div>
                 
             </div>
@@ -16,43 +16,50 @@
                     style="width: 100%"
                 >
                     <el-table-column
-                    prop="code"
-                    label="註冊碼"
-                    sortable
-                    min-width="12"
-                    >
-                    </el-table-column>
-                    <el-table-column
                     prop="account"
-                    label="綁定使用者帳號"
+                    label="帳號"
                     sortable
                     min-width="12"
                     >
                     </el-table-column>
                     <el-table-column
                     prop="name"
-                    label="使用者名稱"
+                    label="姓名"
                     sortable
-                    min-width="12"
+                    min-width="6"
                     >
                     </el-table-column>
                     <el-table-column
-                    prop="way"
-                    label="生成方式"
+                    prop="gender"
+                    label="性別"
                     sortable
-                    min-width="12"
+                    min-width="6"
                     >
                     </el-table-column>
                     <el-table-column
-                    prop="usedTime"
-                    label="使用時間"
+                    prop="birth"
+                    label="生日"
                     sortable
-                    min-width="12"
+                    min-width="8"
                     >
                     </el-table-column>
                     <el-table-column
-                    prop="createTime"
-                    label="產生時間"
+                    prop="organization"
+                    label="機構"
+                    sortable
+                    min-width="8"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                    prop="type"
+                    label="機構類別"
+                    sortable
+                    min-width="7"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                    prop="registerTime"
+                    label="註冊時間"
                     sortable
                     min-width="12"
                     >
@@ -64,6 +71,7 @@
                     >
                     <template #default="scope">
                         <div class="Mtable-row">
+                            <div @click.stop="editUser(scope.row)" class="Mtable-look">查看 -></div>
                             <div class="Mtable-icon-outer">
                                 <img @click.stop="deleteUser(scope.row)" class="Mtable-icon" src="@/assets/images/Icon/delete.svg" alt="close">
                             </div>
@@ -73,25 +81,13 @@
                 </el-table>
             </div>
 
-            <!-- 新增資料 -->
-            <div v-if="isShowAdd" class="popup">
-                <div @click="isShowAdd = false" class="box">
-                    <div @click.stop class="block-box">
-                        <div class="title">新增註冊碼</div>
-                        <div class="item-title">註冊碼數量</div>
-                        <div><input class="input" placeholder="請輸入數量" type="text"></div>
-                        <div class="btn-green">儲存</div>
-                    </div>
-                </div>
-            </div>
-
-
+            
             <!-- 刪除資料 -->
             <div v-if="isShowDelete" class="popup">
                 <div @click="isShowDelete = false" class="box">
                     <div @click.stop class="block-box">
                         <div class="title">刪除提醒</div>
-                        <div class="box-hint">註冊碼刪除後將無法復原，您確認要刪除？</div>
+                        <div class="box-hint">劇本刪除後將無法復原，您確認要刪除？</div>
                         <div class="box-row">
                             <div @click="isShowDelete = false" class="box-cancel">取消</div>
                             <div @click="deleteCurrentData" class="box-delete">刪除</div>
@@ -107,23 +103,28 @@
 <script setup>
 
 const dataAll = reactive([
-  { code: 'awasd', account: 'user1@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
-  { code: 'asdfadsf', account: 'user2@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
-  { code: 'sgg', account: 'user3@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
-  { code: 'jfh', account: 'user4@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
-  { code: 'jdfhd', account: 'user5@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
-  { code: 'rwerer', account: 'user6@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
-  { code: 'asdfadfa', account: 'user7@gmail.com', name: 'frank', way: '管理者建立', usedTime: '2023/03/01 12:22:22', createTime: '2023/03/01 12:22:22' },
+  { account: 'user1@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user2@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user3@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user4@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user5@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user6@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user7@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user8@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
+  { account: 'user9@gmail.com', name: 'frank', gender: '女', birth: '1992/02/01', organization: '森林國小', type: '學校', registerTime: '2022/02/03 12:22:11'},
 ])
 
-const isShowAdd = ref(false)
 const isShowDelete = ref(false)
 
-const addData = ref({})
+const editData = ref({})
 const deleteData = ref({})
 
-function addUser(row) {
-    isShowAdd.value = true
+function addUser() {
+    
+}
+
+function editUser(row) {
+    console.log(row)
 }
 
 function deleteUser(row) {
