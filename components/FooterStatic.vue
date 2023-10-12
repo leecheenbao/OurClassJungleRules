a137397a2735c71f63f174d49d3e4798e23077bc
jeffreylinyu <showeryu18@gmail.com>
2023-10-12 14:19:42 +0800

    1012

--- components/FooterStatic.vue
+++ components/FooterStatic.vue
@@ -9,8 +9,9 @@
         <nuxt-link to="/" class="link-item">龍虎篇</nuxt-link>
       </div> -->
       <div class="content content2">
-        <div class="title">公司</div>
+        <div class="title">幫助</div>
         <nuxt-link to="/about" class="link-item">關於阿普蛙</nuxt-link>
+        <nuxt-link to="/question" class="link-item">常見問題</nuxt-link>
         <nuxt-link to="/connection" class="link-item">聯絡阿普蛙</nuxt-link>
       </div>
       <div class="content content3">
